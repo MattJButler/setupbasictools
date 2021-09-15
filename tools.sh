a54591804a9199dce252c411d10ee87bcc640148
MattJButler <90148954+MattJButler@users.noreply.github.com>
2021-09-15 10:41:40 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,4 +1,4 @@
-#!/bin/bash env bash
+#!/bin/bash
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y
@@ -10,7 +10,13 @@ sudo apt install git -y
 sudo apt install proxychains4 -y
 sudo apt install gobuster -y
 cd /opt && sudo git clone https://github.com/0x-BAD/kali-wordlists.git
+sudo chmod 777 /opt
+cp /opt/kali-wordlists/dirbuster/directory-list-2.3-medium.txt .
+gunzip /opt/kali-wordlists/rockyou.gz
+cp /opt/kali-wordlists/rockyou.txt .
 sudo apt install apache2 -y
 sudo apt install nmap -y
 sudo apt install htop -y
 sudo apt install hydra -y
+sudo apt install john -y
+sudo apt install apache2 -y
