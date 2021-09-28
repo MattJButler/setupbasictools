2473c8e01a531ae532272a1c67a00488b604a0b8
MattJButler <90148954+MattJButler@users.noreply.github.com>
2021-09-28 21:10:51 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -12,6 +12,8 @@ sudo apt install gobuster -y
 cd /opt && sudo git clone https://github.com/0x-BAD/kali-wordlists.git
 sudo chmod 777 /opt
 cp /opt/kali-wordlists/dirbuster/directory-list-2.3-medium.txt .
+cp /opt/kali-wordlists/dirb/small.txt
+cp /opt/kali-wordlists/dirb/big.txt
 gunzip /opt/kali-wordlists/rockyou.gz
 cp /opt/kali-wordlists/rockyou.txt .
 sudo apt install apache2 -y
@@ -20,3 +22,7 @@ sudo apt install htop -y
 sudo apt install hydra -y
 sudo apt install john -y
 sudo apt install apache2 -y
+sudo apt install wireshark -y
+sudo apt install setoolkit -y
+sudo apt install terminator -y
+sudo apt install guake -y
