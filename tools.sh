95a2f8e1712a5bb5ec795b9b5bb7b95bf2444488
MattJButler <90148954+MattJButler@users.noreply.github.com>
2021-09-29 16:03:26 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+$get = "sudo apt install"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y
@@ -26,3 +27,4 @@ sudo apt install wireshark -y
 sudo apt install setoolkit -y
 sudo apt install terminator -y
 sudo apt install guake -y
+sudo apt install binwalk -y
