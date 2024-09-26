72f28514f96588fadf6496e3ac0f82055179ab05
MattJButler <90148954+MattJButler@users.noreply.github.com>
2024-09-26 22:05:03 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -3,6 +3,7 @@ $get = "sudo apt install"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt install sublime-text -y
 sudo apt upgrade -y
 sudo apt install terminator -y
